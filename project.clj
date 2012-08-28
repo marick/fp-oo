4abb6783a5e95eed4cae8c78cbeb0a64084edb7c
Brian Marick <marick@exampler.com>
2012-08-28 09:59:35 -0500

    Add core.match

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject fpoo "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [midje "1.4.0"]])
+                 [midje "1.4.0"]
+                 [org.clojure/core.match "0.2.0-alpha11"]])
