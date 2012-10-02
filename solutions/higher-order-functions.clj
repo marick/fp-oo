570df010dd07dadace52487be2e8e2c54130700a
Brian Marick <marick@exampler.com>
2012-10-02 16:43:06 -0500

    Delete stray file

--- solutions/higher-order-functions.clj
+++ solutions/higher-order-functions.clj
@@ -54,21 +54,6 @@
 (swap! my-atom (constantly 8))
 
 
-;;; 7
-
-(def check-sum
-     (fn [sequence]
-       (apply + (map *
-                     (range 1 (inc (count sequence)))
-                     sequence))))
-
-;;; 8
-
-(def isbn?
-     (fn [candidate]
-       (zero? (rem (check-sum (reversed-digits candidate)) 11))))
-
-
 ;;; 8
 
 (def check-sum
