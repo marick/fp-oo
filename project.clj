2ad34ea81c12888b20e7534d8f6b2b66f9f34d95
Brian Marick <marick@exampler.com>
2012-10-18 16:11:06 -0500

    Can't hurt to be explicit about clojure.algo.monads

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject fpoo "0.1.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [midje "1.4.0"]
+                 [org.clojure/algo.monads "0.1.0"] 
                  [patterned "0.2.0"]])
