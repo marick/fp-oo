2040c42068e405b27b8ec79be6851c535aadf88f
Brian Marick <marick@exampler.com>
2013-03-18 12:15:01 -0500

    Bump project file to latest versions

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject fpoo "0.1.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [midje "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [midje "1.5.0"]
                  [org.clojure/algo.monads "0.1.0"] 
                  [patterned "0.2.0"]])
